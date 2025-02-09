e79e42a777f0a6ff6968e915d80f36558b79e8f6
ADITYA PANDEY <134423207+aditya-pandey-ai@users.noreply.github.com>
2025-02-09 18:15:31 +0530

    Time: 128 ms (13.81%), Space: 38.8 MB (59.29%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.py
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.py
@@ -1,13 +1,17 @@
+from collections import Counter
+
 class Solution:
-    def countBadPairs(self, nums: List[int]) -> int:
+    def countBadPairs(self, nums):
         n = len(nums)
-        total_pairs = n * (n-1)//2
-
-        freq = Counter()
-        good_pairs = 0
-
-        for i,num in enumerate(nums):
-            key = num-i
-            good_pairs += freq[key]
-            freq[key] += 1
-        return total_pairs - good_pairs
+        total_pairs = n * (n - 1) // 2  # Total pairs
+        
+        key_freq = Counter()
+        good_pairs = 0  # Count of good pairs
+        
+        for i, num in enumerate(nums):
+            key = num - i
+            good_pairs += key_freq[key]  # All previous occurrences of key form good pairs
+            key_freq[key] += 1  # Update frequency of current key
+        
+        bad_pairs = total_pairs - good_pairs
+        return bad_pairs
