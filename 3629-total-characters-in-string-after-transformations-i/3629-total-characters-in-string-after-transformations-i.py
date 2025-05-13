4d2fb832dfdafccb98702f8e104f42bef1a45489
ADITYA PANDEY <134423207+aditya-pandey-ai@users.noreply.github.com>
2025-05-13 18:32:23 +0530

    Time: 323 ms (94.12%), Space: 18.3 MB (45.1%) - LeetHub

--- 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.py
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.py
@@ -1,23 +1,16 @@
 class Solution:
-    MOD = 10**9 + 7
     def lengthAfterTransformations(self, s: str, t: int) -> int:
-        freq = [0] * 26
+        MOD = 10**9 + 7
+        data = [0] * 26
 
-        # Initialize frequencies from the input string
         for ch in s:
-            freq[ord(ch) - ord('a')] += 1
+            data[ord(ch) - 97] += 1
+        
+        data = deque(data)
 
-        # Apply transformations t times
         for _ in range(t):
-            new_freq = [0] * 26
-            for i in range(26):
-                if freq[i] == 0:
-                    continue
-                if i == 25:  # 'z'
-                    new_freq[0] = (new_freq[0] + freq[i]) % self.MOD
-                    new_freq[1] = (new_freq[1] + freq[i]) % self.MOD
-                else:
-                    new_freq[i + 1] = (new_freq[i + 1] + freq[i]) % self.MOD
-            freq = new_freq
-
-        return sum(freq) % self.MOD
+            z = data.pop()
+            data.appendleft(z)
+            data[1] += z
+        
+        return sum(data) % MOD
